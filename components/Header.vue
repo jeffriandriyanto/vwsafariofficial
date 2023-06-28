36c3e56eca3f04a28b618619f08418861bb18b25
Creetech Official <creetech.official@gmail.com>
2023-06-28 09:31:08 +0700

    change phone number

--- components/Header.vue
+++ components/Header.vue
@@ -1,49 +1,62 @@
 <template>
   <v-app-bar app :class="bg" elevation="0" style="width: 100vw">
     <client-only>
-    <v-container>
-      <div class="d-flex pa-6 align-center">
-        <img :src="logovw" style="width: 60px" />
-        <v-spacer />
-        <div v-if="smAndDown">
-          <v-menu v-model="menu" :close-on-content-click="false" location="end">
-            <template v-slot:activator="{ props }">
-              <v-btn icon="mdi-menu" v-bind="props" :color="bg === 'white' ? 'primary' : 'white'"> </v-btn>
-            </template>
+      <v-container>
+        <div class="d-flex pa-6 align-center">
+          <img :src="logovw" style="width: 60px" />
+          <v-spacer />
+          <div v-if="smAndDown">
+            <v-menu
+              v-model="menu"
+              :close-on-content-click="false"
+              location="end"
+            >
+              <template v-slot:activator="{ props }">
+                <v-btn
+                  icon="mdi-menu"
+                  v-bind="props"
+                  :color="bg === 'white' ? 'primary' : 'white'"
+                >
+                </v-btn>
+              </template>
 
-            <v-card min-width="300">
-              <v-list>
-                <v-list-item v-for="menu in menus" :href="menu.link" :key="menu.link">
-                  {{ menu.label }}
-                </v-list-item>
-              </v-list>
-            </v-card>
-          </v-menu>
+              <v-card min-width="300">
+                <v-list>
+                  <v-list-item
+                    v-for="menu in menus"
+                    :href="menu.link"
+                    :key="menu.link"
+                  >
+                    {{ menu.label }}
+                  </v-list-item>
+                </v-list>
+              </v-card>
+            </v-menu>
+          </div>
+          <div v-else class="d-flex justify-space-between">
+            <div>
+              <v-btn
+                v-for="menu in menus"
+                :href="menu.link"
+                :key="menu.link"
+                :class="textColor"
+                variant="text"
+                class="text-decoration-none"
+                >{{ menu.label }}</v-btn
+              >
+            </div>
+            <v-btn
+              color="success"
+              variant="flat"
+              prepend-icon="mdi-whatsapp"
+              class="ml-2"
+              rounded
+              @click="openWA"
+              >Whatsapp</v-btn
+            >
+          </div>
         </div>
-        <div v-else class="d-flex justify-space-between">
-        <div>
-          <v-btn
-            v-for="menu in menus"
-            :href="menu.link"
-            :key="menu.link"
-            :class="textColor"
-            variant="text"
-            class="text-decoration-none"
-            >{{ menu.label }}</v-btn
-          >
-        </div>
-          <v-btn
-            color="success"
-            variant="flat"
-            prepend-icon="mdi-whatsapp"
-            class="ml-2"
-            rounded
-            @click="openWA"
-            >Whatsapp</v-btn
-          >
-        </div>
-      </div>
-    </v-container>
+      </v-container>
     </client-only>
   </v-app-bar>
 </template>
@@ -82,8 +95,8 @@ if (process.client) {
 }
 
 const openWA = () => {
-  window.open("https://wa.me/+6281519568726", "_blank");
-}
+  window.open("https://wa.me/+6285877101104", "_blank");
+};
 
 // const scrollMenu = (path) => {
 //   window.scrollTo({ top: `#${path}`, behavior: "smooth" });
--- components/home/HomeOffice.vue
+++ components/home/HomeOffice.vue
@@ -2,15 +2,31 @@
   <a class="anchor" id="contact-us"></a>
   <v-container class="py-12">
     <v-sheet class="pa-4">
-      <v-card-title class="text-black font-weight-bold text-h4 text-center mb-8 d-flex align-center justify-center">
+      <v-card-title
+        class="text-black font-weight-bold text-h4 text-center mb-8 d-flex align-center justify-center"
+      >
         Kontak Kami
       </v-card-title>
       <v-card-text class="text-body-1">
         <v-row>
           <v-col v-for="office in offices" cols="12" sm="4">
-            <a :href="office.url" class="text-decoration-none mb-2" target="_blank">
-              <v-card class="py-4 px-2 text-center" variant="outlined" rounded="xl" color="secondary">
-                <v-icon :icon="office.icon" color="secondary" size="30" class="mb-2" />
+            <a
+              :href="office.url"
+              class="text-decoration-none mb-2"
+              target="_blank"
+            >
+              <v-card
+                class="py-4 px-2 text-center"
+                variant="outlined"
+                rounded="xl"
+                color="secondary"
+              >
+                <v-icon
+                  :icon="office.icon"
+                  color="secondary"
+                  size="30"
+                  class="mb-2"
+                />
                 <div class="text-black text-h6">{{ office.label }}</div>
               </v-card>
             </a>
@@ -25,16 +41,16 @@
 const offices = [
   {
     // url: "tel:+6281519568726",
-    url: "https://wa.me/+6281519568726",
+    url: "https://wa.me/+6285877101104",
     icon: "mdi-phone",
-    label: "+62 815-1956-8726",
+    label: "+62 858-7710-1104",
   },
-//   {
-//     url:
-//       "https://www.google.com/maps/place/Borobudur,+Magelang+Regency,+Central+Java/@-7.6130252,110.0559338,12z/data=!3m1!4b1!4m5!3m4!1s0x2e7a8cef5e6c0f59:0x4027a76e352eb60!8m2!3d-7.6017595!4d110.2119615",
-//     icon: "mdi-pin",
-//     label: "Jl. Borobudur",
-//   },
+  //   {
+  //     url:
+  //       "https://www.google.com/maps/place/Borobudur,+Magelang+Regency,+Central+Java/@-7.6130252,110.0559338,12z/data=!3m1!4b1!4m5!3m4!1s0x2e7a8cef5e6c0f59:0x4027a76e352eb60!8m2!3d-7.6017595!4d110.2119615",
+  //     icon: "mdi-pin",
+  //     label: "Jl. Borobudur",
+  //   },
   {
     url: "mailto:seribuwulung@gmail.com",
     icon: "mdi-email",
@@ -44,6 +60,6 @@ const offices = [
     url: "https://www.instagram.com/vwborobudur_tour/",
     icon: "mdi-instagram",
     label: "VW Borobudur Tour",
-  }
+  },
 ];
 </script>
